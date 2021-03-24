cc60f686a97f622c0fa0fdcbe05b0d95a90493fc
Nhi <conniewongntn@gmail.com>
2021-03-23 20:25:48 -0700

    testing multi user! :D

--- app/src/main/java/com/example/bobabuddy/LoginActivity.java
+++ app/src/main/java/com/example/bobabuddy/LoginActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+// Nhi was here!
 public class LoginActivity extends AppCompatActivity {
 
     Button loginbtn;
